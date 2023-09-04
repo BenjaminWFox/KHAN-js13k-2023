6d6f25bfb2ebbe5559de68f603c8f878c9b5bf59
Ben Fox <BenjaminWFox@gmail.com>
2023-09-04 16:11:52 -0700

    Fix build issue

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
-import { ce, resize } from "./utility";
+import { gei, resize } from "./utility";
 import { GameElements } from "./types";
 import { Game } from "./game";
 import { messages, showMessage } from "./messaging";
 
-const e: GameElements = {
-  board: ce(),
-  title: ce(),
-  game: ce(),
-  new: ce(),
-  continue: ce(),
-  endturn: ce()
-}
-
-window.addEventListener('resize', () => {
-  resize(e);
-})
-
 window.addEventListener('load', () => {
+  const e: GameElements = {
+    board: gei('board')!,
+    title: gei('title')!,
+    game: gei('game')!,
+    new: gei('new')!,
+    continue: gei('continue')!,
+    endturn: gei('endturn')!
+  }
+
   const game = new Game(e)
 
   // Required to prevent awkward zoom-in if double-tapping on mobile
@@ -24,15 +20,15 @@ window.addEventListener('load', () => {
     e.preventDefault();
   }
 
-  Object.keys(e).forEach(key => {
-    e[key] = document.getElementById(key)!;
-  })
-
   e.new.addEventListener('click', () => {
     showMessage(messages.intro, game.newGame.bind(game))
   })
   e.endturn.addEventListener('click', () => game.endPlayerTurn())
   e.continue.addEventListener('click', () => { })
+
+  window.addEventListener('resize', () => {
+    resize(e);
+  })
 
   resize(e);
 
